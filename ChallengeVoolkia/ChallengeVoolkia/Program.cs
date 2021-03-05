34b7e7f4c3febee3aeccffaf4768b19cbf470c03
Francisco Gimenez Andreis <55329447+Frankie239@users.noreply.github.com>
2021-03-05 18:58:16 -0300

    Now the script can make a GET request an have the response string.

---
+++ ChallengeVoolkia/ChallengeVoolkia/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ChallengeVoolkia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetProductsAsync("179571326");
+            Console.ReadKey();
+        }
+
+        private static async Task<string> GetProductsAsync(string seller_id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://api.mercadolibre.com/sites/MLA/search?seller_id=");
+                //client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //This returns the whole list requested from the seller. 
+                string response = await client.GetStringAsync(client.BaseAddress + seller_id);
+
+                //r.ToString();
+                //Console.WriteLine(response);
+                return response;
+            }
+        }
+
+    }
+}
