b3bf6c0543580c960ca62147960d7638a5ebcdf9
Francisco Gimenez Andreis <55329447+Frankie239@users.noreply.github.com>
2021-03-07 01:21:50 -0300

    now it can read data from the API
    Now the request are stored into a list of results to later be used to write the file.

--- ChallengeVoolkia/ChallengeVoolkia/Program.cs
+++ ChallengeVoolkia/ChallengeVoolkia/Program.cs
@@ -3,14 +3,23 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using System.Linq;
 
 namespace ChallengeVoolkia
 {
     class Program
     {
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
-            GetProductsAsync("179571326");
+
+
+            string jsonData = "";
+            //This would not work because the method is ASYNC and there's no await. 
+            jsonData = GetProductsAsync("179571326").ToString();
+            //Console.WriteLine(jsonData);
             Console.ReadKey();
         }
 
@@ -22,27 +31,88 @@ namespace ChallengeVoolkia
         /// <returns></returns>
         private static async Task<string> GetProductsAsync(string seller_id)
         {
+            string response = "";
+            List<Result> itemsBySeller = new List<Result>();
+
+            //create a Idisposable object so it can be throw away when this execution ends. 
             using (var client = new HttpClient())
             {
+                //add a base addres. so this request can be done independent from the seller.
                 client.BaseAddress = new Uri("https://api.mercadolibre.com/sites/MLA/search?seller_id=");
-                //client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Clear();
+                //Accept the data type. 
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
                 //This returns the whole list requested from the seller. 
-                string response = await client.GetStringAsync(client.BaseAddress + seller_id);
-
-                //r.ToString();
-                //Console.WriteLine(response);
-                
-                return response;
+                response = await client.GetStringAsync(client.BaseAddress + seller_id);
             }
+            return response.ToString();
         }
+
+                
+
+                
+                
+                
+                
+
+                
+              
+                
+
+        /// <summary>
+        /// Gets the raw json data and selects it with the Json.Net library.
+        /// </summary>
+        /// <param name="rawJson">the json string substracted from the api request</param>
+        /// <returns></returns>
+        private static async Task<List<Result>> ParseJsonToModel(string rawJson)
+        {
+            List<Result> Results = new List<Result>(); 
+
+            JObject results = JObject.Parse(rawJson);
+
+            for (int i = 0; i < results["results"].Count()-1; i++)
+            {
+                Result parsedObject = new Result()
+                {
+                    id = results["results"][i]["id"].ToString(),
+                    title = results["results"][0]["title"].ToString(),
+                    category_id = results["results"][0]["category_id"].ToString(),
+
+
+                };
+
+                Results.Add(parsedObject);
+            }
+
+            //Console.WriteLine(results["results"][0]["id"]);
+            //Console.WriteLine(results["results"][0]["title"]);
+            //Console.WriteLine(results["results"][0]["category_id"]);
+            //Console.WriteLine(results["results"].Count());
+
+
+
+            //List<Result> restults = (List<Result>)JsonSerializer.Deserialize(splitJson[1], typeof(Result));
+
+            return Results;
+            
+        }
+
+        
         public class Result
         {
-            public string Id { set; get; }
-            public string Title { set; get; }
-            public string Category_id { get; set; }
+            public string id;
 
+            public string title;
+
+            public string category_id;
+           
         }
+
+
+        
+
+
+
     }
 }
